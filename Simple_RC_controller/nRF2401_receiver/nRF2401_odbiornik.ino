8902aae556f6b5938381a696e99a7c85c30092f2
Tomasz Grzmil <tomekg2996@gmail.com>
2021-09-01 20:35:20 +0200

    Send project simpy_RC_controller

---
+++ Simple_RC_controller/nRF2401_receiver/nRF2401_odbiornik.ino
@@ -0,0 +1,104 @@
+#include <SPI.h>
+#include <Servo.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+
+
+#define PWM_FORWARD 5
+#define PWM_BACKWARD 6
+
+//Remember that this code is the same as in the transmitter
+const uint64_t pipeIn = 0xE8E8F0F0E1LL; 
+
+Servo control_servo;
+RF24 radio(9, 10);
+
+int direct, motor_speed, control_servo_value;
+
+ //We define each byte of data input, in this case just 6 channels
+struct Data {
+  byte AxisX;
+  byte AxisY;
+};
+
+Data data;
+
+void resetData(){
+//We define the inicial value of each data input
+//3 potenciometers will be in the middle position so 127 is the middle from 254
+  data.AxisX = 127;
+  data.AxisY = 127;
+}
+
+/**************************************************/
+
+void setup(){
+  Serial.begin(250000); //Set the speed to 9600 bauds if you want.
+  //You should always have the same speed selected in the serial monitor
+  resetData();
+  radio.begin();
+  radio.setAutoAck(false);
+  radio.setDataRate(RF24_250KBPS);
+  radio.openReadingPipe(1,pipeIn);
+  //start the radio comunication
+  radio.startListening();
+
+  control_servo.attach(A0);
+  pinMode(PWM_FORWARD, OUTPUT);
+  pinMode(PWM_BACKWARD, OUTPUT);
+}
+
+/**************************************************/
+
+unsigned long lastRecvTime = 0;
+
+void recvData(){
+  while ( radio.available() ) {
+    radio.read(&data, sizeof(Data));
+    //here we receive the data
+    lastRecvTime = millis();
+  }
+}
+
+/**************************************************/
+
+void loop(){
+  recvData();
+  unsigned long now = millis();
+  //Here we check if we've lost signal, if we did we reset the values 
+  if (now - lastRecvTime > 1000) {
+    // Signal lost?
+    resetData();
+  }
+
+  Serial.print("Axis X: "); Serial.print(data.AxisX);  Serial.print("    ");
+  Serial.print("Axis Y: "); Serial.print(data.AxisY);  Serial.println("    ");
+
+  control_servo_value = abs((data.AxisY / 1.42) - 180);
+  control_servo.write(control_servo_value);
+  motor_speed = (data.AxisX) - 126;
+
+  if(motor_speed > 15){
+    forward(motor_speed);
+  }else if(motor_speed < (-15)){
+    motor_speed = abs(motor_speed);
+    backward(motor_speed);
+  }else{
+    stop_motor();
+  }
+}
+
+void forward(int speed){
+  analogWrite(PWM_FORWARD, speed);
+  analogWrite(PWM_BACKWARD, 0);
+}
+
+void backward(int speed){
+  analogWrite(PWM_BACKWARD, speed);
+  analogWrite(PWM_FORWARD, 0);
+}
+
+void stop_motor(){
+  analogWrite(PWM_BACKWARD,0);
+  analogWrite(PWM_FORWARD,0);
+}
---
+++ Simple_RC_controller/nRF2401_transmitter/nRF2401_nadajnik.ino
@@ -0,0 +1,62 @@
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+
+const uint64_t pipeOut = 0xE8E8F0F0E1LL; //IMPORTANT: The same as in the receiver
+
+RF24 radio(9, 10); // select  CSN  pin
+
+// The sizeof this struct should not exceed 32 bytes
+// This gives us up to 32 8 bits channals
+struct Data {
+  byte AxisX;
+  byte AxisY;
+};
+
+Data data;
+
+void resetData() 
+{
+  //This are the start values of each channal
+  // Throttle is 0 in order to stop the motors
+  //127 is the middle value of the 10ADC.
+    
+  data.AxisX = 127;
+  data.AxisY = 127;
+
+}
+
+void setup()
+{
+  //Start everything up
+  Serial.begin(9600);
+  radio.begin();
+  radio.setAutoAck(false);
+  radio.setDataRate(RF24_250KBPS);
+  radio.openWritingPipe(pipeOut);
+  resetData();
+}
+
+/**************************************************/
+
+// Returns a corrected value for a joystick position that takes into account
+// the values of the outer extents and the middle of the joystick range.
+int mapJoystickValues(int val, int lower, int middle, int upper, bool reverse)
+{
+  val = constrain(val, lower, upper);
+  if ( val < middle )
+    val = map(val, lower, middle, 0, 128);
+  else
+    val = map(val, middle, upper, 128, 255);
+  return ( reverse ? 255 - val : val );
+}
+
+void loop()
+{
+  // The calibration numbers used here should be measured 
+  // for your joysticks till they send the correct values.
+  data.AxisX = mapJoystickValues( analogRead(A2), 0, 512, 1024, true );
+  data.AxisY = mapJoystickValues( analogRead(A3),  0, 512, 1024, true );
+
+  radio.write(&data, sizeof(Data));
+}
